dd6afff1d29882dc99e3f959c1bfda3ea02138b3
prozdeveloperweb <142255802+prozdeveloperweb@users.noreply.github.com>
2023-12-06 18:50:18 -0300

    criacao tela login

--- loginAppAna/app/src/main/java/com/anavitoria/loginappana/MainActivity.java
+++ loginAppAna/app/src/main/java/com/anavitoria/loginappana/MainActivity.java
@@ -3,12 +3,35 @@ package com.anavitoria.loginappana;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
+    Button loginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (username.getText().toString().equals("pedro") && password.getText().toString().equals("1234")){
+                    Toast.makeText(MainActivity.this, "Login realizado com sucesso!!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Usuário ou Senha incorretos!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
